3cdff8682162d3518b0a2a394b37f8bf07eec477
erebuseternal <marcelsanders96@gmail.com>
2016-01-08 15:47:16 -0500

    Added code to digest solr schema prior to turning the schema into create table statements. The code allows you to input a file, extract the file, and then process the file, wherein fieldtype names and their data types, field names and their data types and the unique key is found

--- typehandler.py
+++ typehandler.py
@@ -123,7 +123,149 @@ class Type(Enum):
         else:
             return keep_value
 
+def getTypeEnum(self, string):
+    # so this is going to take a string name, and convert it to
+    # the enum
+    if string == 'Bool':
+        return Type.Bool
+    elif string == 'DateRange':
+        return Type.DateRange
+    elif string == 'Str':
+        return Type.Str
+    elif string == 'Text':
+        return Type.Text
+    elif string == 'TrieDouble'
+        return Type.TrieDouble
+    elif string == 'TrieInt':
+        return Type.TrieInt
+    elif string == 'TrieLong':
+        return Type.TrieLong
+    elif string == 'TrieFloat':
+        return Type.TrieFloat
+
 """
 Next we are going to have some code here to read in schema files and turn them
 into CREATE TABLE statements!!!
 """
+
+from xmlextractor import XMLExtractor
+import re
+
+class SolrSchemaIngestor:
+    """
+    This class takes a solr schema and first extracts the schema,
+    and then processes it for easy use.
+    """
+
+    def __init__(self, directory=''):
+        self.extractor = XMLExtractor(directory)
+        self.fieldTypeExpression = re.compile('"?solr.([^ ]*)Field"?') # used for
+                        # finding the group that holds the type name
+
+    def InputFile(self, file_name):
+        self.extractor.InputFile(file_name)
+
+    def Extract(self):
+        # we will both extract the document here and set it to be the class'
+        # document
+        self.extractor.Extract()
+        self.document = self.extractor.document
+
+    def Process(self):
+        """
+        Okay so this is going to do a couple things:
+        1. It's going to determine the fieldTypes, namely it will create a
+        dictionary fieldTypes that will contain the name given to each type
+        and then the actual data type enum as a value
+        2. It's going to determine the fields, namely it will create a
+        dictionary fields that will contain the name of the field and then
+        the data type enum corresponding. Note because the fieldType names are
+        what let us know the data type for the fields we have to do #1 first
+        3. Discover the key
+
+        We will just call three functions to do these things
+        """
+        self.DiscoverFieldTypes()
+        self.DiscoverFields()
+        self.DiscoverKey()
+
+    def DiscoverFieldTypes(self):
+        """
+        This is going to run through the nodes in self.document
+        and its going to find all the field types and record the
+        name as a key of a dictionary and the type enum as the value
+        """
+        # we do this by calling an recursive function
+        self.fieldTypes = {}
+        for node in self.document.nodes:
+            self.grabFieldTypes(node)
+
+    def grabFieldTypes(self, node):
+        # this looks to see if it is a fieldType, then we assign key value
+        # pair to self.fieldTypes with the key being the name and the value
+        # being the type enum. If it isn't a fieldType we look at its children
+        # if it has any
+        if node.name == 'fieldType':
+            type_string = self.getTypeName(node.attributes['class'])
+            type_enum = getTypeEnum(type_string)
+            self.fieldTypes[node.attributes['name'][1:-1]] = type_enum
+            # we get rid of the quotes on the attribute with the [1:-1]
+        else:
+            for child in node.children:
+                self.grabFieldTypes(child)
+
+    def getTypeName(self, class_attr):
+        # uses a regular expression to catch the name from a string like
+        # solr.TextField or "solr.TextField"
+        match = re.search(self.fieldTypeExpression, class_attr)
+        return match(1) # return the group holding the name we are looking for
+
+    """
+    Now that we can discover the field types we want to be able to link the
+    actual fields to their types (this will be useful in creating tables with
+    Phoenix). So assuming the fieldTypes dictionary has been filled we call
+    the next function to grab field names and the corresponding types
+    """
+    def DiscoverFields(self):
+        # same as discover types in how this works
+        # the dictionary we end up with is field names as keys and the type enum
+        # as the value
+        self.fields = {}
+        for node in self.document.nodes:
+            self.grabFields(node)
+
+    def grabFields(self, node):
+        # this looks to see if it is a field. If it is it grabs its name and
+        # then, using the type attribute and the fieldTypes dictionary that
+        # has already been established, it gets the type too. And puts these
+        # two things as key and value in our fields dictionary
+        if node.name == 'field':
+            # first we get the name
+            name = node.attributes['name'][1:-1]    # get rid of quotes too
+            # next we get the type
+            type_name = node.attributes['type'][1:-1]
+            # next get the corresponding type enum from this name
+            type_enum = self.fieldTypes[type_name]
+            # and now we store key and value
+            self.fields[name] = type_enum
+        else:
+            for child in node.children:
+                self.grabFields(child)
+
+    """
+    Finally we need to know the unique key (if there is one)
+    """
+
+    def DiscoverKey(self):
+        # we will use the same approach as the other discover functions
+        self.key = None
+        for node in self.document.nodes:
+            self.grabKey(node)
+
+    def grabKey(self, node):
+        # same idea as the other grab methods
+        if node.name == 'uniqueKey':
+            self.key = node.content.strip()
+        else:
+            for child in node.children:
+                self.grabKey(child)
