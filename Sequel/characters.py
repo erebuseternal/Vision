26f261b66ddd85fc53dac328e25aa738bf86e44e
erebuseternal <marcelsanders96@gmail.com>
2016-01-15 14:46:48 -0500

    Wrote up object for Select and Delete commands :) gotta go!

--- Sequel/characters.py
+++ Sequel/characters.py
@@ -215,6 +215,9 @@ class QueryField:
     def SetName(self, name):
         self.name = name
 
+    def RemoveName(self):
+        self.name = None
+
     def SetField(self, field_name, as_name=True):
         # this sets the content as a field name. If as_name is true
         # it also sets name to the field_name
@@ -234,7 +237,10 @@ class QueryField:
         self.type = 'FUNCTION'
 
     def SetTable(self, table):
-        self.table = table
+        if isinstance(table, QueryTable)
+            self.table = table
+        else:
+            raise Issue('the table you attempted to enter is not an instance of QueryTable')
 
     def AddKeyword(self, keyword):
         self.keywords.append(keyword)
@@ -245,7 +251,10 @@ class QueryField:
     def prepareField(self):
         # this puts everything around the field that's needed
         if self.table:
-            product = '%s.%s' % (self.table, self.content)
+            if self.table.alias:
+                product = '%s.%s' % (self.content, self.table.alias)
+            else:
+                product = '%s.%s' % (self.content, self.table.name)
         else:
             product = self.content
         for keyword in self.keywords
@@ -254,11 +263,395 @@ class QueryField:
 
     def __str__(self):
         if self.type == 'FUNCTION':
-            return '%s(%s) AS %s' % (self.content['function'], self.prepareField(self.content['argument'], self.name)
+            if self.name:
+                return '%s(%s) AS %s' % (self.content['function'], self.prepareField(self.content['argument']), self.name)
+            else:
+                return '%s(%s)' % (self.content['function'], self.prepareField(self.content['argument']))
         elif self.type == 'VALUE':
-            return '%s AS %s' % (self.content, self.name)
+            if self.name:
+                return '%s AS %s' % (self.content, self.name)
+            else:
+                return '%s' % self.content  # this is here to handle fields used
+                                            # in conditions which don't need names
         elif self.type == 'FIELD':
             if self.name == self.content:
                 return self.prepareField(self.content)
             else:
-                return '%s AS %s' % (self.prepareField(content), self.name)
+                if self.name:
+                    return '%s AS %s' % (self.prepareField(content), self.name)
+                else:
+                    return self.prepareField(content)
+
+"""
+Tables. Tables have two parts, the table name and then the table alias.
+So this will be pretty simple
+"""
+
+class QueryTable:
+    name = None
+    alias = None
+
+    def SetName(self, name):
+        self.name = name
+
+    def SetAlias(self, alias):
+        self.alias = alias
+
+    def RemoveAlias(self):
+        self.alias = None
+
+    def __str__(self):
+        if self.alias:
+            return '%s AS %s' % (self.name, self.alias)
+        else:
+            return self.name
+
+"""
+For the where statement we have conditional statements. And we are also going
+to have some grouping of those statments. So the question becomes: how do we
+want to handle this?
+
+Well for where and having we have as elements an expression that is composed
+of two 'values' and a comparison operator. Now the values are either a field,
+a function, or a constant value. So we can just use query fields! So we will
+have two fields and an operator
+"""
+
+class QueryCondition:
+    first_operand = None
+    second_operand = None
+    operator = None
+    addition_state = 1
+
+    def AddOperator(self, operator):
+        self.operator = operator
+
+    def AddOperator(self, operand1, operand2=None):
+        if not isinstance(operand1, QueryField):
+            raise Issue('first operand entered is not of type QueryField')
+        # now we quickly remove the name of the operand1 (if it has one)
+        operand1.RemoveName()
+        if operand2 == None
+            if addition_state == 1:
+                self.first_operand = operand1
+                self.addition_state = 2
+            else:
+                self.second_operand = operand1
+                self.addition_state = 1
+        else:
+            if not isinstance(operand2, QueryField):
+                raise Issue('second operand entered is not of type QueryField')
+            # remove name from operand2
+            operand2.RemoveName()
+            self.first_operand = operand1
+            self.second_operand = operand2
+            self.addition_state = 1
+
+    def AddFirstOperand(self, operand):
+        self.first_operand = operand
+        self.addition_state = 2
+
+    def AddSecondOperand(self, operand):
+        self.second_operand = operand
+        self.addition_state = 1
+
+    def __str__(self):
+        return '%s %s %s' (self.first_operand, self.operator, self.second_operand)
+
+"""
+Finally, just to keep things pretty, we need to handle order by fields.
+These are simply query fields and then an optional keyword
+"""
+
+class QueryOrder:
+    field = None
+    keyword = None
+
+    def SetKeyword(self, keyword):
+        self.keyword = keyword
+
+    def RemoveKeyword(self):
+        self.keyword = None
+
+    def SetField(self, field):
+        if not isinstance(field, QueryField):
+            raise Issue('field entered is not an instance of QueryField')
+        self.field = field
+
+    def __str__(self):
+        if self.field.name: # if there is a name we want to use it
+            if self.keyword:
+                return '%s %s' % (self.field.name, self.keyword)
+            else:
+                return self.field.name
+        else:   # otherwise we want the print like it would be in a conditional statement
+            if self.keyword:
+                return '%s %s' % (self.field, self.keyword)
+            else:
+                return '%s' % self.field
+
+"""
+So we are now ready to put write the select statement object! Remembering from
+above:
+
+SELECT
+    * fields
+    * From
+        * table names
+    * Where
+        * conditional statements
+        * logical joins and groupings
+    * GroupBy
+        * field names
+    * Having
+        * conditional statements
+    * OrderBy
+        * field names with optional ASC, DSC
+
+So we will have a fields list, a table list, a groupby list and an order by list.
+Then for where and having we are going to do something a little different. We
+are going to represent them by a set of nodes.
+
+The idea is the following: each top level node will either have a QueryCondition
+in it, or will be empty. If it is empty it will have children, each of the children
+being part of one group that needs to get treated first. The children of this
+node act like a new level, so there can be groups inside. But this is how
+we are going to work with the conditional stuff. Finally, each node that has a
+next sibling is going to have an operator as well. This is how the two siblings
+are joined.
+"""
+
+class Node:
+    parent = None
+    children = []
+    sibling = None # assumed to be the next sibling
+    operator = None
+    condition = None
+
+    def SetCondition(self, condition):
+        if not isinstance(condition, QueryCondition):
+            raise Issue('entered condition is not an instance of QueryCondition')
+        self.condition = condition
+
+    def CreateChild(self, operator='AND'):  # note the default operator is AND
+                                            # this default is only used if we are
+                                            # adding a child to a parent with children
+        child = Node()
+        child.parent = self
+        if len(self.children) > 0:
+            self.children[-1].sibling = child
+            self.chilrend[-1].operator = operator
+        self.children.append(child)
+        return child
+
+    def CreateSibling(self, operator='AND'):
+        if self.parent
+            return self.parent.CreateChild(operator)
+        else:
+            sibling = Node()
+            self.sibling = sibling
+            self.operator = operator
+            return sibling
+
+    def SetOperator(self, operator):
+        self.operator = operator
+
+    # now the convenience method. This method will
+    # construct a string using itself, its children,
+    # its siblings and the operators for all. It starts
+    # from itself and goes from there.
+    # I'm going to do this by creating a function that
+    # prints stuff for the node itself and then calls
+    # itself on its first child and its sibling. And then joins
+    # the result as needed
+    def createString(self):
+        if not self.sibling:
+            if self.condition:
+                return '%s' % self.condition
+            else:
+                children_string = self.children[0].createString()
+                return '(%s)' % children_string
+        else:
+            if not self.operator:
+                raise Issue('operator is missing and this node has a sibling')
+            next_string = self.sibling.createString()
+            if self.condition:
+                return '%s %s %s' % (self.condition, self.operator, next_string)
+            else:
+                children_string = self.children[0].createString()
+                return '(%S) %s %s' % (children_string, self.operator, next_string)
+
+    def __str__(self):
+        return self.createString()
+
+class Select(Character):
+    fields = []
+    tables = []
+    orderby = []
+    groupby = []
+    where = None
+    last_added_where_node = None
+    having = None
+    last_added_having_node = None
+
+    def AddField(self, field):
+        if not isinstance(field, QueryField):
+            raise Issue('entered field is not an instance of QueryField')
+        self.fields.append(field)
+
+    def AddTable(self, table):
+        if not isinstance(table, QueryTable):
+            raise Issue('entered table is not an instance of QueryTable')
+        self.tables.append(table)
+
+    def AddGroupingField(self, field):
+        if not isinstance(table, QueryField):
+            raise Issue('entered field is not an instance of QueryField')
+        # have to get rid of the name if there is one
+        field.RemoveName()
+        self.groupby.append(field)
+
+    def AddOrderBy(self, orderby):
+        if not isinstance(orderby, QueryOrder):
+            raise Issue('entered orderby is not an instance of QueryOrder')
+        self.orderby.append(orderby)
+
+    def AddWhereCondition(self, condition, operator='AND'):
+        # operator is only used once at least one condition is present in the where
+        # and gets attached to the last added condition
+        if not self.where:
+            new_node = Node()
+            new_node.SetCondition(condition)
+            self.where = new_node
+            self.last_added_where_node = condition
+        else:
+            if self.last_added_where_node.condition:
+                # create a sibling for the last node
+                new_node = self.last_added_where_node.CreateSibling()
+            else:   # this is the case where the last one created was a group
+                new_node = self.last_added_where_node.CreateChilde()
+            # set the nodes condition
+            new_node.SetCondition(condition)
+            # set the operator on the last condition
+            self.last_added_where_node.SetOperator(operator)
+            self.last_added_where_node = new_node
+
+
+    def AddWhereGroup(self, operator='AND'):
+        self.AddWhereCondition(None, operator)
+
+    def ExitWhereGroup(self):
+        # all we have to do is go back up a level
+        self.last_added_where_node = self.last_added_where_node.parent
+
+    def AddHavingCondition(self, condition, operator='AND'):
+        # operator is only used once at least one condition is present in the where
+        # and gets attached to the last added condition
+        if not self.having:
+            new_node = Node()
+            new_node.SetCondition(condition)
+            self.having = new_node
+            self.last_added_having_node = condition
+        else:
+            if self.last_added_having_node.condition:
+                # create a sibling for the last node
+                new_node = self.last_added_having_node.CreateSibling()
+            else:   # this is the case where the last one created was a group
+                new_node = self.last_added_having_node.CreateChilde()
+            # set the nodes condition
+            new_node.SetCondition(condition)
+            # set the operator on the last condition
+            self.last_added_having_node.SetOperator(operator)
+            self.last_added_having_node = new_node
+
+
+    def AddHavingGroup(self, operator='AND'):
+        self.AddHavingCondition(None, operator)
+
+    def ExitHavingGroup(self):
+        self.last_added_having_node = self.last_added_having_node.parent
+
+    def Speak(self):
+        # first we start with the fields
+        select_text = 'SELECT'
+        for field in self.fields:
+            select_text = '%s %s,' % (select_text, field)
+        select_text = select_text[:-1]  # shave off last comma
+        # next we do tables
+        from_text = '\nFROM'
+        for table in self.tables:
+            from_text = '%s %s,' % (from_text, table)
+        from_text = from_text[:-1]
+        # next we handle the where clause
+        where_text = ''
+        if self.where:
+            where_text = '\nWHERE %s' % (self.where)  # invoking that cool string generation
+                                                    # method for nodes
+        # next we handle the groupby clause
+        groupby_text = ''
+        if len(self.groupby) > 0:
+            groupby_text = '\nGROUPBY'
+            for field in self.groupby:
+                grouby_text = '%s %s,' % (grouby_text, field)
+            groupby_text = groupby_text[:-1]
+        # next having
+        having_text = ''
+        if self.having:
+            having_text = '\nHAVING %s' % self.having
+        # finally orderby
+        orderby_text = ''
+        if len(self.orderby) > 0:
+            orderby_text = '\nORDERBY'
+            for field in self.orderby:
+                orderby_text = '%s %s,' % (orderby_text, field)
+            orderby_text = orderby_text[:-1]
+        # and now we put it all together
+        return select_text + from_text + where_text + grouby_text + having_text + orderby_text
+
+"""
+And now for delete
+"""
+
+class Delete(Character):
+    table = None
+    where = None
+    last_added_where_node = None
+
+    def SetTable(self, table):
+        if not isinstance(table, QueryTable):
+            raise Issue('entered table is not an instance of QueryTable')
+        self.table = table
+
+    def AddWhereCondition(self, condition, operator='AND'):
+        # operator is only used once at least one condition is present in the where
+        # and gets attached to the last added condition
+        if not self.where:
+            new_node = Node()
+            new_node.SetCondition(condition)
+            self.where = new_node
+            self.last_added_where_node = condition
+        else:
+            if self.last_added_where_node.condition:
+                # create a sibling for the last node
+                new_node = self.last_added_where_node.CreateSibling()
+            else:   # this is the case where the last one created was a group
+                new_node = self.last_added_where_node.CreateChilde()
+            # set the nodes condition
+            new_node.SetCondition(condition)
+            # set the operator on the last condition
+            self.last_added_where_node.SetOperator(operator)
+            self.last_added_where_node = new_node
+
+
+    def AddWhereGroup(self, operator='AND'):
+        self.AddWhereCondition(None, operator)
+
+    def ExitWhereGroup(self):
+        # all we have to do is go back up a level
+        self.last_added_where_node = self.last_added_where_node.parent
+
+    def Speak(self):
+        if self.where:
+            return 'DELETE FROM %s WHERE %s' % (self.table, self.where)
+        else:
+            return 'DELETE FROM %s' % self.table
