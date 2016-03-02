15a997b14e05fdfeb20926199c7e727ee4d03ca9
Marcel Sanders <marcelsanders96@gmail.com>
2016-03-01 21:41:32 -0500

    Removed stuff I don't intend on using for a while

--- test.py
+++
@@ -1,9 +0,0 @@
-from fieldextractor import *
-
-fieldextractor = FieldExtractor('')
-fieldextractor.InputFile('SolrData/Bear')
-fieldextractor.ExtractFields()
-fieldtracker = FieldTracker()
-fieldtracker.InputFile('page.html')
-fieldtracker.Process()
-print joinData(fieldextractor, fieldtracker)
